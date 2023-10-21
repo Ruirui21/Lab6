5dc1f20ce873c90773598c8e996610d1c39c1943
Rui Jiang <rui.jiang2007@gmail.com>
2023-10-21 16:14:00 -0400

    This is Rui's encoder code

---
+++ new.py
@@ -0,0 +1,16 @@
+def encode(password):
+    newpassword=""
+    for i in range(8):
+        newpassword=newpassword+str((int(password[i])+3)%10)
+    return newpassword
+if __name__=="__main__":
+    while(True):
+        print("Encoder menu")
+        print("0. Quit")
+        print("1. Encode Password")
+        menu_option=input("Select a Menu Option: ")
+        if menu_option=="0":
+            break
+        elif menu_option=="1":
+            password=input("Enter a password to encode: ")
+            print("Your encoded password is "+encode(password))
